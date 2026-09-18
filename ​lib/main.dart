7516ad1386863692c849dd6ec69e99ac625731e4
ahmed7886a-cyber <ahmed7886a@gmail.com>
2026-09-18 09:33:32 +0300

    Create main.dart

---
+++ ​lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alwadi App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Alwadi App Initialized Successfully'),
+        ),
+      ),
+    );
+  }
+}
